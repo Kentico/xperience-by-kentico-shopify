5f669dcc1a7730522e57ae26527ae99d7750efa1
Martin Kyjac <martin.kyjac@bluesoft.cz>
2024-04-24 09:42:07 +0200

    Create shopping cart page view models

---
+++ examples/DancingGoat-Shopify/Models/WebPage/Shopify/ShoppingCartPage/ShoppingCartTotalsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DancingGoat.Models.WebPage.Shopify.ShoppingCartPage
+{
+    public class ShoppingCartTotalsViewModel
+    {
+        public string GrandTotal { get; set; }
+        public string CouponInsertionMessage { get; set; }
+        public string NextStepUrl { get; set; }
+        public string NextStepName { get; set; }
+        public IEnumerable<string> AppliedCoupons { get; set; }
+    }
+}

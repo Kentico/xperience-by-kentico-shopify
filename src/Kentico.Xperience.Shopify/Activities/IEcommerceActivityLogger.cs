1826b2b5005e1b818148c144efc5d1fe90cc00ab
martinkyjac <martin.kyjac@bluesoft.cz>
2024-04-26 13:59:47 +0200

    Add interface IEcommerceActivityLogger

---
+++ src/Kentico.Xperience.Shopify/Activities/IEcommerceActivityLogger.cs
@@ -0,0 +1,40 @@
+﻿using Kentico.Xperience.Shopify.ShoppingCart;
+using ShopifySharp.GraphQL;
+
+namespace Kentico.Xperience.Shopify.Activities
+{
+    public interface IEcommerceActivityLogger
+    {
+        /// <summary>
+        /// Logs activity product added to shopping cart. 
+        /// </summary>
+        /// <param name="cartItem">New shopping cart item that was added to the shopping cart.</param>
+        /// <param name="quantity">Cart item quantity.</param>
+        void LogProductAddedToShoppingCartActivity(ShoppingCartItem cartItem, int quantity);
+
+
+        /// <summary>
+        /// Logs activity product removed to shopping cart. 
+        /// </summary>
+        /// <param name="cartItem">Shopping cart item that was removed from the shopping cart.</param>
+        /// <param name="quantity">Cart item quantity.</param>
+        void LogProductRemovedFromShoppingCartActivity(ShoppingCartItem cartItem, int quantity);
+
+
+        /// <summary>
+        /// Logs purchase activity.
+        /// </summary>
+        /// <param name="totalPrice">Order total price in shopping cart currency.</param>
+        /// <param name="orderId">Order ID.</param>
+        /// <param name="currency">Shopping cart currency.</param>
+        void LogPurchaseActivity(decimal totalPrice, long orderId, CurrencyCode currency);
+
+
+        /// <summary>
+        /// Logs product purchased activity.
+        /// </summary>
+        /// <param name="cartItem">Purchased shopping cart item.</param>
+        /// <param name="quantity">Quantity.</param>
+        void LogPurchasedProductActivity(ShoppingCartItem cartItem, int quantity);
+    }
+}

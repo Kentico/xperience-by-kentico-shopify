04e7803a13ee14f7292d8be159ee0a45c97c5e3a
Martin Kyjac <martin.kyjac@bluesoft.cz>
2024-04-29 13:34:38 +0200

    Rename folder from ShopifyCartWidget to ShopifyCartViewComponent

---
+++ examples/DancingGoat-Shopify/Components/ViewComponents/ShopifyCartViewComponent/ShopifyCartViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using DancingGoat.Components.ViewComponents.ShopifyCartWidget;
+using Kentico.Xperience.Shopify.ShoppingCart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DancingGoat.Components.ViewComponents
+{
+    public class ShopifyCartViewComponent : ViewComponent
+    {
+        private readonly IShoppingService shoppingService;
+
+        public ShopifyCartViewComponent(
+            IShoppingService shoppingService)
+        {
+            this.shoppingService = shoppingService;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cart = await shoppingService.GetCurrentShoppingCart();
+            var model = new ShopifyCartWidgetViewModel()
+            {
+                CartItemCount = cart?.Items.Sum(x => x.Quantity) ?? 0,
+                CartUrl = DancingGoatConstants.SHOPPING_CART_PATH
+            };
+
+            return View($"~/Components/ViewComponents/ShopifyCartWidget/Default.cshtml", model);
+        }
+    }
+}

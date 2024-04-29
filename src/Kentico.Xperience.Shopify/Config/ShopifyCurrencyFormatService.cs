2a0c0b91fbc629bb2c808744db1a64bdc38e94dd
Martin Kyjac <martin.kyjac@bluesoft.cz>
2024-04-29 09:35:14 +0200

    Use inject service IInfoProvider<CurrencyFormatInfo>

--- src/Kentico.Xperience.Shopify/Config/ShopifyCurrencyFormatService.cs
+++ src/Kentico.Xperience.Shopify/Config/ShopifyCurrencyFormatService.cs
@@ -1,4 +1,5 @@
-﻿using CMS.Helpers;
+﻿using CMS.DataEngine;
+using CMS.Helpers;
 
 using Kentico.Xperience.Shopify.Admin;
 
@@ -7,14 +8,17 @@ namespace Kentico.Xperience.Shopify.Config
     internal class ShopifyCurrencyFormatService : IShopifyCurrencyFormatService
     {
         private readonly IProgressiveCache cache;
+        private readonly IInfoProvider<CurrencyFormatInfo> currencyFormatInfoProvider;
 
-        public ShopifyCurrencyFormatService(IProgressiveCache cache)
+
+        public ShopifyCurrencyFormatService(IProgressiveCache cache, IInfoProvider<CurrencyFormatInfo> currencyFormatInfoProvider)
         {
             this.cache = cache;
+            this.currencyFormatInfoProvider = currencyFormatInfoProvider;
         }
 
         public IReadOnlyDictionary<string, string> GetFormats() =>
-            cache.Load(cs => CurrencyFormatInfo.Provider.Get()
+            cache.Load(cs => currencyFormatInfoProvider.Get()
                 .ToDictionary(format => format.CurrencyCode, format => format.CurrencyPriceFormat)
             , new CacheSettings(20, $"{nameof(ShopifyCurrencyFormatService)}|{nameof(GetFormats)}")
             {

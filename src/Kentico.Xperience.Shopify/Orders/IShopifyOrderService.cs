1503854944666d5d32c404445402ffe47ddcd640
Martin Kyjac <martin.kyjac@bluesoft.cz>
2024-04-29 13:29:59 +0200

    Create shopify order service

---
+++ src/Kentico.Xperience.Shopify/Orders/IShopifyOrderService.cs
@@ -0,0 +1,7 @@
+﻿namespace Kentico.Xperience.Shopify.Orders
+{
+    public interface IShopifyOrderService
+    {
+        Task<OrderCustomerDetails?> GetOrderCustomerDetails(string cartToken);
+    }
+}
---
+++ src/Kentico.Xperience.Shopify/Orders/ShopifyOrderService.cs
@@ -0,0 +1,48 @@
+﻿using Kentico.Xperience.Shopify.Config;
+using Kentico.Xperience.Shopify.Products;
+using ShopifySharp;
+using ShopifySharp.Factories;
+using ShopifySharp.Filters;
+
+namespace Kentico.Xperience.Shopify.Orders
+{
+    internal class ShopifyOrderService : ShopifyServiceBase, IShopifyOrderService
+    {
+        private readonly IOrderService orderService;
+
+
+        public ShopifyOrderService(IOrderServiceFactory orderServiceFactory, IShopifyIntegrationSettingsService integrationSettingsService) : base(integrationSettingsService)
+        {
+            orderService = orderServiceFactory.Create(shopifyCredentials);
+        }
+
+        public async Task<OrderCustomerDetails?> GetOrderCustomerDetails(string cartToken)
+        {
+            var filter = new OrderListFilter()
+            {
+                CreatedAtMin = DateTime.Now.AddMinutes(-10).Date,
+                Fields = "customer"
+            };
+
+            var result = await orderService.ListAsync(filter);
+
+            var customer = result.Items.FirstOrDefault(x => x.CartToken == cartToken)?.Customer;
+            if (customer == null)
+            {
+                return null;
+            }
+
+
+            return new OrderCustomerDetails()
+            {
+                Email = customer.Email,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Address1 = customer.DefaultAddress.Address1,
+                City = customer.DefaultAddress.City,
+                Zip = customer.DefaultAddress.Zip,
+                CountryCode = customer.DefaultAddress.CountryCode
+            };
+        }
+    }
+}

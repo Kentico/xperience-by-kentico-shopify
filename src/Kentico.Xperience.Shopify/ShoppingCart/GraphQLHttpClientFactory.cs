a73b50582bd7ed96a82c4709dd863bf152ccb309
Martin Kyjac <martin.kyjac@bluesoft.cz>
2024-05-29 13:34:28 +0200

    Add Storefront API Buyer IP header to GraphQLHttpClient

--- src/Kentico.Xperience.Shopify/ShoppingCart/GraphQLHttpClientFactory.cs
+++ src/Kentico.Xperience.Shopify/ShoppingCart/GraphQLHttpClientFactory.cs
@@ -2,15 +2,23 @@
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
 
+using Microsoft.AspNetCore.Http;
+
 namespace Kentico.Xperience.Shopify.ShoppingCart
 {
     internal class GraphQLHttpClientFactory : IGraphQLHttpClientFactory
     {
         private readonly HttpClient httpClient;
 
-        public GraphQLHttpClientFactory(IHttpClientFactory httpClientFactory)
+        public GraphQLHttpClientFactory(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
         {
             httpClient = httpClientFactory.CreateClient(ShopifyConstants.STOREFRONT_API_CLIENT_NAME);
+            string? buyerIP = httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString();
+
+            if (!string.IsNullOrEmpty(buyerIP))
+            {
+                httpClient.DefaultRequestHeaders.Add(ShopifyConstants.STOREFRONT_API_BUYER_IP_NAME, buyerIP);
+            }
         }
 
         /// <inheritdoc/>

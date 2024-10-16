310ffa15561028dac0a148cfe21be0cee0ef2ac1
Martin Kyjac <martin.kyjac@bluesoft.cz>
2024-10-16 13:48:56 +0200

    Upgrade xbk version and add dancing goat store template changes

--- examples/DancingGoat-Shopify/Models/Reusable/Tag/TagViewModel.cs
+++ examples/DancingGoat-Shopify/Models/Reusable/Tag/TagViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using CMS.ContentEngine;
-using Tag = CMS.ContentEngine.Tag;
+﻿using Tag = CMS.ContentEngine.Tag;
 
 namespace DancingGoat.Models
 {
